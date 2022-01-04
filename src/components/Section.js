45a43f3a33e5f7ca37c139d9c2ba8b97fcf38b9d
David Li <dvdli2323@gmail.com>
2022-01-04 13:31:13 -0500

    added heading and resume/contact buttons

--- src/components/Section.js
+++ src/components/Section.js
@@ -1,11 +1,24 @@
 import React from 'react';
 import styled from 'styled-components';
-import IcelandImage from '../assets/Iceland.jpg';
+import {createGlobalStyle} from "styled-components";
+import IcelandImage from '../assets/Iceland.jpeg';
 
 const Section = () => {
   return (
     <Wrap>
-      Section
+      <GlobalStyle />
+      <ItemText>
+        <h1>David Li</h1>
+        <h2>Software Engineer</h2>
+      </ItemText>
+      <ButtonGroup>
+        <LeftButton>
+          RESUME
+        </LeftButton>
+        <RightButton>
+          CONTACT
+        </RightButton>
+      </ButtonGroup>
     </Wrap>
   )
 }
@@ -16,5 +29,48 @@ const Wrap = styled.div`
   width: 100vw;
   height: 100vh;
   background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
   background-image: url(${IcelandImage});
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
 `
+const ItemText = styled.div`
+  padding-top: 30vh;
+  text-align: center;
+`
+
+const ButtonGroup = styled.div`
+  display: flex;
+  margin-bottom: 30px;
+`
+
+const LeftButton = styled.div`
+  color: white;
+  font-size: 12;
+  background-color: #171A20CC;
+  height: 40px;
+  width: 256px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-radius: 100px;
+  opactiy: 0.75;
+  cursor: pointer;
+`
+
+const RightButton = styled(LeftButton)`
+`
+
+const GlobalStyle = createGlobalStyle`
+  h1 {
+    font-size: 40px;
+    font-weight: 700;
+  }
+  h2 {
+    font-size: 14px;
+    font-weight: 400;
+  }
+`;
