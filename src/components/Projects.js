8309062c66574f88b9bf1bd151fe60b57515ea9b
David Li <dvdli2323@gmail.com>
2022-01-05 15:43:09 -0500

    buttons for github and live link

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import styled from 'styled-components';
 import {createGlobalStyle} from "styled-components";
+import TagdDemo from '../assets/TagdHostDemo2.gif';
 import PeachStudioDemo from '../assets/PeachStudioDemo1.gif';
 import ForzaDemo from '../assets/ForzaDemo1.gif';
-import TagdDemo from '../assets/TagdHostDemo1.gif';
+import WhiteCloudDemo from '../assets/WhiteCloudDemo.gif';
+
 
 const Projects = () => {
   return (
@@ -14,32 +16,73 @@ const Projects = () => {
           <h2 id='projects-header'>Projects</h2>
         </ProjectsTitle>
         <ProjectsGroup>
-        <ProjectInfo>
+          <ProjectInfo>
             <ProjectImg>
             <img src={TagdDemo} alt='Tagd' id='tagd-img' />
             </ProjectImg>
             <ProjectText>
               <h4 id='project-title'>Tagd</h4>
-              <p id='project-description'>Augmented reality mobile application that allow hosts to create and display personalized messages to help guests access and locate necessities in and around their property</p>
+              <p id='project-description'>Augmented reality mobile application targeted for Airbnb hosts to to help their guests access and locate necessities in and around their property.</p>
+              <ButtonGroup>
+                <LeftButton>
+                  GitHub
+                </LeftButton>
+                <RightButton>
+                  Set Up
+                </RightButton>
+              </ButtonGroup>
             </ProjectText>
           </ProjectInfo>
+
           <ProjectInfo>
             <ProjectText>
               <h4 id='left-title'>Peach Studio</h4>
-              <p id='left-description'>An e-commerce website specializing in furniture to make your home .... sweet home</p>
+              <p id='left-description'>An e-commerce website specializing in furniture to make your home .... sweet home.</p>
+              <ButtonGroup id='left-button'>
+                <LeftButton>
+                  GitHub
+                </LeftButton>
+                <RightButton>
+                  Live
+                </RightButton>
+              </ButtonGroup>
             </ProjectText>
             <ProjectImg>
             <img src={PeachStudioDemo} alt='peachStudio' id='project-img' />
             </ProjectImg>
           </ProjectInfo>
+
           <ProjectInfo>
             <ProjectImg>
             <img src={ForzaDemo} alt='forza' id='project-img' />
             </ProjectImg>
             <ProjectText>
               <h4 id='project-title'>Forza</h4>
-              <p id='project-description'>Mobile and web application that allow users to help track their progress at the gym using speech recognition</p>
+              <p id='project-description'>Mobile and web application where users can track their strength progress at the gym using speech recognition.</p>
+              <ButtonGroup>
+                <LeftButton>
+                  GitHub
+                </LeftButton>
+                <RightButton>
+                  Live
+                </RightButton>
+              </ButtonGroup>
             </ProjectText>
+          </ProjectInfo>
+
+          <ProjectInfo>
+            <ProjectText>
+              <h4 id='left-title'>White Cloud</h4>
+              <p id='left-description'>Platform that provides a user the opportunity to discover music and learn and play the piano instantly.</p>
+              <ButtonGroup id='left-button'>
+                <LeftButton>
+                  Coming Soon
+                </LeftButton>
+              </ButtonGroup>
+            </ProjectText>
+            <ProjectImg>
+            <img src={WhiteCloudDemo} alt='whiteCloud' id='project-img' />
+            </ProjectImg>
           </ProjectInfo>
         </ProjectsGroup>
       </Wrap>
@@ -51,7 +94,7 @@ export default Projects;
 
 const Wrap = styled.div`
   width: 100vw;
-  height: 155vh;
+  height: 200vh;
   background-color: #000;
   display: flex;
   flex-direction: column;
@@ -83,6 +126,35 @@ const ProjectText = styled.div`
   margin: 0px 70px 0px 70px;
 `
 
+const ButtonGroup = styled.div`
+  display: flex;
+  @media (max-width: 768px) {
+    flex-direction: column;
+  }
+`
+
+const LeftButton = styled.div`
+  color: #F5F5F5;
+  font-size: 12px;
+  height: 35px;
+  width: 195px;
+  background-color: transparent;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-radius: 100px;
+  border-style: solid;
+  border-width: 2px;
+  border-color: #F5F5F5;
+  cursor: pointer;
+  margin: 5px;
+`
+
+const RightButton = styled(LeftButton)`
+  color: #D0D1D2;
+  border-color: #393C41;
+`
+
 const GlobalStyle = createGlobalStyle`
   #projects-header {
     font-size: 28px;
@@ -95,7 +167,7 @@ const GlobalStyle = createGlobalStyle`
   #project-title {
     color: white;
     font: 20px;
-    padding: 32px 0px 8px;
+    padding: 0px 0px 16px;
   }
   #project-description {
     color: #E2E4E4;
@@ -104,17 +176,20 @@ const GlobalStyle = createGlobalStyle`
     width: 30vw;
   }
   #tagd-img {
-    width: 32%;
+    width: 25%;
   }
   #left-title {
     color: white;
     font: 20px;
-    padding: 32px 0px 8px 16px;
+    padding: 0px 0px 16px 16px;
   }
   #left-description {
     color: #E2E4E4;
     font-size: 14px;
     padding: 0px 0px 16px 16px;
     width: 30vw;
+  }
+  #left-button {
+    padding: 0px 0px 12px 12px;
   }
 `
