f2f80682dfb39bae17d345a724440db309ec7674
David Li <dvdli2323@gmail.com>
2022-01-06 11:18:51 -0500

    icons on left

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,51 @@
-import React from 'react'
+import React from 'react';
+import styled from 'styled-components';
+import {createGlobalStyle} from "styled-components";
 
 const Header = () => {
   return (
-    <div>
-      David Li
-    </div>
+    <Container>
+      <GlobalStyle />
+      <LeftIcons>
+        <a href="https://www.linkedin.com/in/davdli/">
+          <i class="devicon-github-original"></i>
+        </a>
+        <a href="https://github.com/davdli">
+          <i class="devicon-linkedin-plain"></i>
+        </a>
+      </LeftIcons>
+      <Menu>
+      </Menu>
+    </Container>
   )
 }
 
-export default Header
+export default Header;
+
+const Container = styled.div`
+  min-height: 60px;
+  position: fixed;
+  display: flex;
+  align-tems: center;
+  padding: 0 20px;
+`
+
+const LeftIcons = styled.div``
+
+const Menu = styled.div``
+
+const GlobalStyle = createGlobalStyle`
+  .devicon-github-original {
+    font-size: 36px;
+    color: #F4F4F4;
+    margin: 0px 10px 0px 10px;
+  }
+  .devicon-linkedin-plain {
+    font-size: 36px;
+    color: #F4F4F4;
+    margin: 0px 10px 0px 10px;
+  }
+  a {
+    text-decoration: none;
+  }
+`
