e9cfcb4e8f7910579fb6e017fac7f8288430f44d
David Li <dvdli2323@gmail.com>
2022-01-08 23:45:20 -0500

    contact section update

--- src/components/Tech.js
+++ src/components/Tech.js
@@ -1,122 +1,134 @@
 import React from 'react';
 import styled from 'styled-components';
 import {createGlobalStyle} from "styled-components";
+import Fade from 'react-reveal/Fade';
 
 const Tech = () => {
   return (
     <div>
       <Wrap id='technologies'>
         <GlobalStyle />
-        <TitleText>
-          <h2 id='tech-header'>Technologies</h2>
-        </TitleText>
-        <TechGroup>
-          <TechIcon className='tech-icon'>
-              <TechName>
-                JavaScript
-              </TechName>
-              <i class="devicon-javascript-plain colored"></i>
-          </TechIcon>
-          <TechIcon className='tech-icon'>
-              <TechName>
-                HTML5
-              </TechName>
-              <i class="devicon-html5-plain colored"></i>
-          </TechIcon>
-          <TechIcon className='tech-icon'>
-              <TechName>
-                CSS3
-              </TechName>
-              <i class="devicon-css3-plain colored"></i>
-          </TechIcon>
-          <TechIcon className='tech-icon'>
-              <TechName>
-                Python
-              </TechName>
-              <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/python/python-original.svg" class="devicon-python-plain" alt="python" />
-          </TechIcon>
-        </TechGroup>
+        <Fade bottom>
+          <TitleText>
+            <h2 id='tech-header'>Technologies</h2>
+          </TitleText>
+        </Fade>
 
-        <TechGroup>
-          <TechIcon className='tech-icon'>
-              <TechName>
-                Node.js
-              </TechName>
-              <i class="devicon-nodejs-plain colored"></i>
-          </TechIcon>
-          <TechIcon className='tech-icon'>
-              <TechName>
-                React
-              </TechName>
-              <i class="devicon-react-original colored"></i>
-          </TechIcon>
-          <TechIcon className='tech-icon'>
-              <TechName>
-                Express
-              </TechName>
-              <i class="devicon-express-original colored"></i>
-          </TechIcon>
-          <TechIcon className='tech-icon'>
-              <TechName>
-                PostgreSQL
-              </TechName>
-              <i class="devicon-postgresql-plain colored"></i>
-          </TechIcon>
-        </TechGroup>
+        <Fade clear>
+          <TechGroup>
+            <TechIcon className='tech-icon'>
+                <TechName>
+                  JavaScript
+                </TechName>
+                <i class="devicon-javascript-plain colored"></i>
+            </TechIcon>
+            <TechIcon className='tech-icon'>
+                <TechName>
+                  HTML5
+                </TechName>
+                <i class="devicon-html5-plain colored"></i>
+            </TechIcon>
+            <TechIcon className='tech-icon'>
+                <TechName>
+                  CSS3
+                </TechName>
+                <i class="devicon-css3-plain colored"></i>
+            </TechIcon>
+            <TechIcon className='tech-icon'>
+                <TechName>
+                  Python
+                </TechName>
+                <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/python/python-original.svg" class="devicon-python-plain" alt="python" />
+            </TechIcon>
+          </TechGroup>
+        </Fade>
 
-        <TechGroup>
-          <TechIcon className='tech-icon'>
-              <TechName>
-                Sequelize
-              </TechName>
-              <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/sequelize/sequelize-original.svg" class="devicon-sequelize-plain" alt="sequelize"/>
-          </TechIcon>
-          <TechIcon className='tech-icon'>
-              <TechName>
-                Redux
-              </TechName>
-              <i class="devicon-redux-original colored"></i>
-          </TechIcon>
-          <TechIcon className='tech-icon'>
-              <TechName>
-                Material UI
-              </TechName>
-              <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/materialui/materialui-original.svg" class="devicon-materialui-plain" alt="materialui" />
-          </TechIcon>
-          <TechIcon className='tech-icon'>
-              <TechName>
-                Firebase
-              </TechName>
-              <i class="devicon-firebase-plain colored"></i>
-          </TechIcon>
-        </TechGroup>
+        <Fade clear>
+          <TechGroup>
+            <TechIcon className='tech-icon'>
+                <TechName>
+                  Node.js
+                </TechName>
+                <i class="devicon-nodejs-plain colored"></i>
+            </TechIcon>
+            <TechIcon className='tech-icon'>
+                <TechName>
+                  React
+                </TechName>
+                <i class="devicon-react-original colored"></i>
+            </TechIcon>
+            <TechIcon className='tech-icon'>
+                <TechName>
+                  Express
+                </TechName>
+                <i class="devicon-express-original colored"></i>
+            </TechIcon>
+            <TechIcon className='tech-icon'>
+                <TechName>
+                  PostgreSQL
+                </TechName>
+                <i class="devicon-postgresql-plain colored"></i>
+            </TechIcon>
+          </TechGroup>
+        </Fade>
 
-        <TechGroup>
-          <TechIcon className='tech-icon'>
-              <TechName>
-                Heroku
-              </TechName>
-              <i class="devicon-heroku-plain colored"></i>
-          </TechIcon>
-          <TechIcon className='tech-icon'>
-              <TechName>
-                Git
-              </TechName>
-              <i class="devicon-git-plain colored"></i>
-          </TechIcon>
-          <TechIcon className='tech-icon'>
-              <TechName>
-                Mocha
-              </TechName>
-              <i class="devicon-mocha-plain colored"></i>
-          </TechIcon>
-          <TechIcon className='tech-icon'>
-              <TechName>
-                Figma
-              </TechName>
-              <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/figma/figma-original.svg" alt='figma' class='devicon-figma'/>
-          </TechIcon>
-        </TechGroup>
+        <Fade clear>
+          <TechGroup>
+            <TechIcon className='tech-icon'>
+                <TechName>
+                  Sequelize
+                </TechName>
+                <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/sequelize/sequelize-original.svg" class="devicon-sequelize-plain" alt="sequelize"/>
+            </TechIcon>
+            <TechIcon className='tech-icon'>
+                <TechName>
+                  Redux
+                </TechName>
+                <i class="devicon-redux-original colored"></i>
+            </TechIcon>
+            <TechIcon className='tech-icon'>
+                <TechName>
+                  Material UI
+                </TechName>
+                <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/materialui/materialui-original.svg" class="devicon-materialui-plain" alt="materialui" />
+            </TechIcon>
+            <TechIcon className='tech-icon'>
+                <TechName>
+                  Firebase
+                </TechName>
+                <i class="devicon-firebase-plain colored"></i>
+            </TechIcon>
+          </TechGroup>
+        </Fade>
+
+        <Fade clear>
+          <TechGroup>
+            <TechIcon className='tech-icon'>
+                <TechName>
+                  Heroku
+                </TechName>
+                <i class="devicon-heroku-plain colored"></i>
+            </TechIcon>
+            <TechIcon className='tech-icon'>
+                <TechName>
+                  Git
+                </TechName>
+                <i class="devicon-git-plain colored"></i>
+            </TechIcon>
+            <TechIcon className='tech-icon'>
+                <TechName>
+                  Mocha
+                </TechName>
+                <i class="devicon-mocha-plain colored"></i>
+            </TechIcon>
+            <TechIcon className='tech-icon'>
+                <TechName>
+                  Figma
+                </TechName>
+                <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/figma/figma-original.svg" alt='figma' class='devicon-figma'/>
+            </TechIcon>
+          </TechGroup>
+        </Fade>
       </Wrap>
     </div>
   )
